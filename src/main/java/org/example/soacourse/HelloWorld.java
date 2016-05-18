663136d12ceda9c754ca8eaef5e3ff898db38683
Sergi L <slalaguna@Sergis-MacBook-Pro.local>
2016-05-18 11:53:08 +0200

    empty change

--- src/main/java/org/example/soacourse/HelloWorld.java
+++ src/main/java/org/example/soacourse/HelloWorld.java
@@ -14,9 +14,6 @@ public class HelloWorld {
     @Path("/echo/{input}")
     @Produces("text/plain")
     public String ping(@PathParam("input") String input) {
-    	
-    	
-    	
         return input;
     }
 
